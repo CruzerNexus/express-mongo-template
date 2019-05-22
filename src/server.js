9b2dfadc5afb7d464b933ef01209dff6a80f2c8e
Greg Cruz <therealcruzer@outlook.com>
2019-05-21 19:51:57 -0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { connectDatabase } = require('./database');
+const usersController = require('./controllers/users');
+
+connectDatabase();
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/users', usersController);
+
+app.get('/', (req, res) => {
+  res.send({dog: 'ace'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening at localhost:${port}...`);
+});
+
